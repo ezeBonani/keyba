cc42d00416f7816bb4c7b34f0675da6c9bdb546c
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-05-13 20:57:38 -0300

    fix apiRequest baseURL

--- api/client/src/lib/apiRequest.js
+++ api/client/src/lib/apiRequest.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.BACKEND_URL || "http://localhost:8800/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8800/api",
   withCredentials: true,
 });
+
+console.log(import.meta.env.VITE_BACKEND_URL);
 
 export default apiRequest;
