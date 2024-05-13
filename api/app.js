453723883bd21f52e3ac394f65d3364f4983b4c6
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-05-13 17:52:36 -0300

    add pre installation for render

--- api/app.js
+++ api/app.js
@@ -5,8 +5,11 @@ import userRoute from "./routes/user.route.js";
 import mailRoute from "./routes/mail.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(cookieParser()); //middleware para las cookies
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); //middleware para los CORS
@@ -16,6 +19,14 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/mail", mailRoute);
+
+//console.log(path.join(__dirname, "/client/dist"));
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 app.listen(8800, () => {
   console.log("Server is running!");
